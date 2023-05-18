7accc67ee08bae1234a8f8d3dcb23b1cbab42410
Anders-RM <Anders_RMathiesen@pm.me>
2023-05-18 20:34:41 +0200

    test

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ import subprocess
 subprocess.run(['sudo', 'pacman', '-Syu'])
 
 # Install packages
-subprocess.run(['sudo', 'pacman', '-S', '--noconfirm', 'libreoffice', 'oh-my-zsh', 'yay', 'pamac'])
+subprocess.run(['sudo', 'pacman', '-S', '--noconfirm', 'libreoffice-fresh', 'oh-my-zsh', 'yay', 'pamac'])
+subprocess.run(['sudo', 'yay', '-S', '--noconfirm','pamac-all'])
+
 
 # Replace KDE application launcher widget with application menu
 subprocess.run(['kwriteconfig5', '--file', '~/.config/plasma-org.kde.plasma.desktop-appletsrc', '--group', 'Containments', '--key', 'org.kde.plasma.desktop.defaultPanel', '--type', 'string', 'com.canonical.menu.kicker'])
